3263849b1503e0f5245369f3d77272259a8bc555
Bang Uhyeok <0105bang@gmail.com>
2024-06-27 14:49:14 +0900

    section3 - ~chapter4

---
+++ section3/src/chapter2.ts
@@ -0,0 +1,10 @@
+//unknown 에는 어떤 값이든 넣을 수 있다.
+//가장 상위 집합이기 때문
+
+function unknownExam() {
+  let a: unknown = 1;
+  let b: unknown = "hello";
+  let c: unknown = true;
+  let d: unknown = null;
+  let e: unknown = undefined;
+}
---
+++ section3/src/chapter4.ts
@@ -0,0 +1,3 @@
+let a: string | number;
+a = 1;
+a = "hello";
