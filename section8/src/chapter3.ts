bae2cf400a753f05197589c3eed4f5ba6affb48b
Bang Uhyeok <0105bang@gmail.com>
2024-07-04 23:27:00 +0900

    section8 ~chatper3

---
+++ section8/src/chapter3.ts
@@ -0,0 +1,66 @@
+/**
+ * 맵드 타입
+ * mapped type
+ */
+
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+
+// 유저 조회
+function fetchUser(): User {
+  return {
+    id: 1,
+    name: "soono",
+    age: 10,
+  };
+}
+
+// 유저 수정
+function updateUser(user: User) {
+  // 수정하는 기능
+}
+
+// updateUser({age: 15}) // ❌, { age: 15 }는 User 타입이 아니므로 오류 발생
+
+type PartialUser = {
+  id?: string;
+  name?: string;
+  age?: number;
+};
+
+function updateUser2(user: PartialUser) {
+  // 수정하는 기능
+}
+
+updateUser2({ age: 15 });
+
+// 맵드 타입
+type PartialUser2 = {
+  [key in keyof User]?: User[key];
+};
+
+// keyof User === "id" | "name" | "age"
+
+function updateUser3(user: PartialUser2) {
+  // 수정하는 기능
+}
+
+updateUser3({ age: 15 });
+
+type ReadonlyUser = {
+  readonly [key in keyof User]: User[key];
+};
+
+function fetchUser2(): ReadonlyUser {
+  return {
+    id: 1,
+    name: "soono",
+    age: 10,
+  };
+}
+
+const user = fetchUser2();
+// user.id = 1; 읽기 전용이라 오류남
